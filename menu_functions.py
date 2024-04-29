a17ff0e7efd8680e945fefbeb60d3ac70040780f
bradx32 <bradx32@hotmail.com>
2024-04-29 21:45:40 +1000

    Implemented remove function

--- menu_functions.py
+++ menu_functions.py
@@ -7,8 +7,26 @@ def add_results(file_name):
         writer.writerow([todo_name, "False"]) # Do grocery,False
 
 
-def remove_results():
-    print("Remove results")
+def remove_results(file_name):
+    todo_name = input("Enter the todo name that you want to delete: ")
+    # Create a new list
+    todo_lists = []
+    # Put all the previous items into the list except the one they want to delete
+    with open(file_name, "r") as f:
+        reader = csv.reader(f)
+        is_exist = False
+        for row in reader: # [do grocery,False]
+            if (todo_name != row[0]): # do laundry != do grocery -> True
+                todo_lists.append(row) # [ [do grocery,False], [complete assignment,False] ]
+            else:
+                is_exist = True
+    if not is_exist:
+        print("No item with that name exists.")
+
+    # Write the enter aquarium_list.csv file with this new list
+    with open(file_name, "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(todo_lists)
 
 def mark_results():
     print("Mark results")
