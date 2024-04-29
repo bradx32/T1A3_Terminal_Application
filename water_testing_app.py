05c63b1ff7643b3fe7fa633524647b3590c4002c
bradx32 <bradx32@hotmail.com>
2024-04-29 18:51:35 +1000

    First function and files created

---
+++ water_testing_app.py
@@ -0,0 +1,14 @@
+
+
+print("Welcome to the Water Testing application")
+print("\n")
+
+def create_menu(): # Function #1 
+    print("1. Enter 1 to add item to the list")
+    print("2. Enter 2 to remove item from the list")
+    print("3. Enter 3 to mark any item as complete")
+    print("4. Enter 4 to view Water Testing results")
+    print("5. Enter 5 to exit application")
+
+    user_choice = input("\n Enter your selection: ")
+    return user_choice
