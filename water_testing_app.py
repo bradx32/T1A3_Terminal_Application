d5c40f2c504af01b269a10b1af08e393a8754e8b
bradx32 <bradx32@hotmail.com>
2024-04-29 19:19:32 +1000

    results_list auto creation if doesn't exist

--- water_testing_app.py
+++ water_testing_app.py
@@ -1,3 +1,9 @@
+# System packages
+import os.path 
+
+# External packages
+
+# Imports of our own fucntiones
 from menu_functions import add_results, remove_results, mark_results, view_results
 
 print("Welcome to the Water Testing application")
@@ -13,7 +19,17 @@ def create_menu(): # Function #1
     user_choice = input("\n Enter your selection: ")
     return user_choice
 
-file_name = "list.csv"
+file_name = "results_list.csv"
+
+# if the file does not exist
+if (not os.path.isfile(file_name)):
+    print("A list file was created as it did not exist, this will store your input results")
+    # Create the file
+    todo_file = open(file_name, "w")
+    # Enter the headings into the file
+    todo_file.write("title,completed")
+    # Close the file
+    todo_file.close()
 
 choice = ""
 
