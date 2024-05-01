bf5b28dc4374be9065d3597130b78d5f2454a35c
bradx32 <bradx32@hotmail.com>
2024-05-01 20:32:57 +1000

    Testing new code for add_test_results

--- menu_functions.py
+++ menu_functions.py
@@ -1,13 +1,28 @@
 import csv
 
 
-def add_results(file_name):
-    results_name = input("Enter tank name") # Do grocery
+def add_aquarium(file_name):
+    results_name = input("Enter Aquarium name: ")
     with open(file_name, "a") as f:
         writer = csv.writer(f)
-        writer.writerow([results_name]) # Do grocery
+        writer.writerow([results_name])
 
-# create a dictionary for the "Aquarium name" added when using option 1 
+
+
+# def add_test_results(file_name):
+#     results_name = {
+#         "Date:": input("Enter date: "),
+#         "PH:": input("Enter PH: "),
+#         "Ammonia:": input("Enter Ammonia: "),
+#         "Nitrite:": input("Enter Nitrite: "),
+#         "Nitrate:": input("Enter Nitrate: ")
+#     }
+#     with open(file_name, "a") as f:
+#         writer = csv.writer(f)
+#         writer.writerow([results_name])
+
+
+
 
 
 def remove_results(file_name):
@@ -18,9 +33,9 @@ def remove_results(file_name):
     with open(file_name, "r") as f:
         reader = csv.reader(f)
         is_exist = False
-        for row in reader: # [do grocery,False]
-            if (results_name != row[0]): # do laundry != do grocery -> True
-                todo_lists.append(row) # [ [do grocery,False], [complete assignment,False] ]
+        for row in reader:
+            if (results_name != row[0]):
+                todo_lists.append(row) 
             else:
                 is_exist = True
     if not is_exist:
@@ -32,35 +47,29 @@ def remove_results(file_name):
         writer.writerows(todo_lists)
 
 
-
-
-
-def mark_results(file_name):
-    results_name = input("Enter the todo name that you want to mark as complete: ")
-    todo_lists = []
-    with open(file_name, "r") as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if (results_name != row[0]):
-                todo_lists.append(row)
-            else:
-                todo_lists.append(row[0], "True")
-    with open(file_name, "w") as f:
-        writer = csv.writer(f)
-        writer.writerows(todo_lists)
-
-
-
-
 def view_results(file_name):
     try:
-        with open(file_name, "r") as f: #open in read mode ("r")
+        with open(file_name, "r") as f: 
             reader = csv.reader(f)
-            reader.__next__() # this will skip the first lane/title of csv file.
-            for row in reader:
-                if (row[1 == "True"]):
-                    print(f"{row[0]} is complete")
-                else:
-                    print(f"{row[0]} is not complete")
+            reader.__next__() 
     except FileNotFoundError:
-        print("The Aquarium List file does not exist.")
+        print("The Aquarium results_table file does not exist.")
+
+
+
+
+
+
+# def mark_results(file_name):
+#     results_name = input("Enter the todo name that you want to mark as complete: ")
+#     todo_lists = []
+#     with open(file_name, "r") as f:
+#         reader = csv.reader(f)
+#         for row in reader:
+#             if (results_name != row[0]):
+#                 todo_lists.append(row)
+#             else:
+#                 todo_lists.append(row[0], "True")
+#     with open(file_name, "w") as f:
+#         writer = csv.writer(f)
+#         writer.writerows(todo_lists)
--- water_testing_app.py
+++ water_testing_app.py
@@ -6,15 +6,16 @@ from rich import print # Lesson video at 2:34:00 watch from here if needing help
 
 
 # Imports of our own fucntiones
-from menu_functions import add_results, remove_results, mark_results, view_results
+from menu_functions import add_aquarium, add_test_results, remove_results, view_results
 
-print("Welcome to the Water Testing application \U0001F9EA \U0001F41F ")
+print("Welcome to the Aquarium Water Testing results application \U0001F9EA \U0001F41F ")
+print("Here you can name your Aquarium, store test results and view by date")
 print("\n")
 
 def create_menu(): # Function #1 
-    print("1. Enter 1 to add item to the list")
-    print("2. Enter 2 to remove item from the list")
-    print("3. Enter 3 to mark any item as complete")
+    print("1. Enter 1 to name your Aquarium")
+    print("2. Enter 2 to add test results to your Aquarium")
+    print("2. Enter 3 to remove item from the list")
     print("4. Enter 4 to view Water Testing results")
     print("5. Enter 5 to exit application")
 
@@ -30,7 +31,7 @@ if (not os.path.isfile(file_name)):
     # Create the file
     aquarium_name_file = open(file_name, "w")
     # Enter the headings into the file
-    aquarium_name_file.write("Aquarium Name,Date Tested,PH,Ammonia(PPM),Nitrite(PPM),Nitrate(PPM)\n")
+    aquarium_name_file.write("Aquarium Name\n")
     # Close the file
     aquarium_name_file.close()
 
@@ -40,13 +41,13 @@ while choice != "5":
     choice = create_menu()
 
     if (choice == "1"):
-        add_results(file_name)
-
+        add_aquarium(file_name)
+        
     elif (choice == "2"):
-        remove_results(file_name)
+        add_test_results(file_name)
 
     elif (choice == "3"):
-        mark_results(file_name)
+        remove_results(file_name)
 
     elif (choice == "4"):
         view_results(file_name)
